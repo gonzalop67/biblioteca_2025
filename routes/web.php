61cfd7120650bffaa3a9a756925e212f8c553c0f
Gonzalo Peñaherrera E. <gonzalop67@gmail.com>
2025-09-29 13:53:26 -0500

    Blade - Parte 3 - AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\PermisoController;
 use App\Http\Controllers\InicioController;
-
-// Route::get('/', function () {
-//     return view('inicio');
-// });
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', [InicioController::class, 'index']);
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function(){
+    Route::get('permiso', [PermisoController::class, 'index'])->name('permiso');
+    Route::get('permiso/crear', [PermisoController::class, 'create'])->name('permiso.crear');
+});
