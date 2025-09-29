0c334d7655a12e0ad2944718edae80a991f2efaf
Gonzalo Peñaherrera E. <gonzalop67@gmail.com>
2025-09-29 10:01:56 -0500

    Implementación del contenido AdminLTE a Blade

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InicioController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('inicio');
+// });
+
+Route::get('/', [InicioController::class, 'index']);
