816e1734c034bf12274a7f7d41243d9a99c7e905
Gonzalo Peñaherrera E. <gonzalop67@gmail.com>
2025-09-30 14:46:55 -0500

    Sistema de Menús - Parte 4 - CRUD - Validación desde el cliente

--- resources/views/theme/lte/layout.blade.php
+++ resources/views/theme/lte/layout.blade.php
@@ -64,6 +64,10 @@
     <!-- AdminLTE App -->
     <script src="{{asset("assets/$theme/dist/js/adminlte.min.js") }}"></script>
 
+    <script src="{{asset("assets/js/jquery-validation/jquery.validate.min.js")}}"></script>
+    <script src="{{asset("assets/js/jquery-validation/localization/messages_es.min.js")}}"></script>
+    <script src="{{asset("assets/js/funciones.js")}}"></script>
+
     @yield('scripts')
 </body>
 
